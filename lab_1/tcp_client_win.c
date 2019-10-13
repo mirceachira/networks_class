7c50d07b2b984dba73d6150f9efb46bb8cd6d757
mirceachira <chira.mircea.mc@gmail.com>
2019-10-13 11:55:40 +0300

    tcp win/nix socket type trail 1

--- lab_1/tcp_client_win.c
+++ lab_1/tcp_client_win.c
@@ -1,48 +1,102 @@
-#define WIN32_LEAN_AND_MEAN
-#define _WIN32_WINNT  0x501
+#ifdef _WIN32
+  #ifndef _WIN32_WINNT
+    #define _WIN32_WINNT 0x0501  /* Windows XP fix found */
+  #endif
+  #include <winsock2.h>
+  #include <Ws2tcpip.h>
+  #include <windows.h>
+  #include <winsock2.h>
+  #include <ws2tcpip.h>
 
-#include <windows.h>
-#include <winsock2.h>
-#include <ws2tcpip.h>
+  // Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
+  #pragma comment (lib, "Ws2_32.lib")
+  #pragma comment (lib, "Mswsock.lib")
+  #pragma comment (lib, "AdvApi32.lib")
+#else
+  /* Assume that any non-Windows platform uses POSIX-style sockets instead. */
+  #include <sys/socket.h>
+  #include <arpa/inet.h>
+  #include <netdb.h>  /* Needed for getaddrinfo() and freeaddrinfo() */
+  #include <unistd.h> /* Needed for close() */
+
+  typedef int SOCKET;
+  int INVALID_SOCKET = -1, SOCKET_ERROR=-1;
+#endif
+
 #include <stdlib.h>
 #include <stdio.h>
-
-
-// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
-#pragma comment (lib, "Ws2_32.lib")
-#pragma comment (lib, "Mswsock.lib")
-#pragma comment (lib, "AdvApi32.lib")
-
+#include <string.h>
 
 #define DEFAULT_BUFLEN 512
 #define DEFAULT_PORT "27015"
 
-int __cdecl main(int argc, char **argv)
+int sockInit(void)
 {
-    WSADATA wsaData;
+  #ifdef _WIN32
+    WSADATA wsa_data;
+    return WSAStartup(MAKEWORD(1,1), &wsa_data);
+  #else
+    return 0;
+  #endif
+}
+
+int sockQuit(void)
+{
+  #ifdef _WIN32
+    return WSACleanup();
+  #else
+    return 0;
+  #endif
+}
+
+int sockClose(SOCKET sock)
+{
+
+  int status = 0;
+
+  #ifdef _WIN32
+    status = shutdown(sock, SD_BOTH);
+    if (status == 0) { status = closesocket(sock); }
+  #else
+    status = shutdown(sock, SHUT_RDWR);
+    if (status == 0) { status = close(sock); }
+  #endif
+
+  return status;
+
+}
+
+
+
+int main(int argc, char **argv)
+{
+    int iResult;
+    // Windows needs initial socket config
+    if (sockInit() != 0) {
+        printf("WSAStartup failed with error: %d\n", iResult);
+        return 1;
+    } else {
+        printf("Initial config OK!\n");
+    }
+
     SOCKET ConnectSocket = INVALID_SOCKET;
     struct addrinfo *result = NULL,
                     *ptr = NULL,
                     hints;
     char *sendbuf = "this is a test";
     char recvbuf[DEFAULT_BUFLEN];
-    int iResult;
     int recvbuflen = DEFAULT_BUFLEN;
 
     // Validate the parameters
     if (argc != 2) {
         printf("usage: %s server-name\n", argv[0]);
         return 1;
+    } else {
+        printf("arguments success!\n");
     }
 
-    // Initialize Winsock
-    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
-    if (iResult != 0) {
-        printf("WSAStartup failed with error: %d\n", iResult);
-        return 1;
-    }
 
-    ZeroMemory( &hints, sizeof(hints) );
+    memset( &hints, 0, sizeof(hints) );
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP;
@@ -51,8 +105,10 @@ int __cdecl main(int argc, char **argv)
     iResult = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result);
     if ( iResult != 0 ) {
         printf("getaddrinfo failed with error: %d\n", iResult);
-        WSACleanup();
+        sockQuit();
         return 1;
+    } else {
+        printf("get address success!\n");
     }
 
     // Attempt to connect to an address until one succeeds
@@ -62,15 +118,17 @@ int __cdecl main(int argc, char **argv)
         ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
             ptr->ai_protocol);
         if (ConnectSocket == INVALID_SOCKET) {
-            printf("socket failed with error: %ld\n", WSAGetLastError());
-            WSACleanup();
+            printf("socket failed\n");
+            sockQuit();
             return 1;
+        } else {
+            printf("socket init success!\n");
         }
 
         // Connect to server.
         iResult = connect( ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
         if (iResult == SOCKET_ERROR) {
-            closesocket(ConnectSocket);
+            sockClose(ConnectSocket);
             ConnectSocket = INVALID_SOCKET;
             continue;
         }
@@ -81,29 +139,24 @@ int __cdecl main(int argc, char **argv)
 
     if (ConnectSocket == INVALID_SOCKET) {
         printf("Unable to connect to server!\n");
-        WSACleanup();
+        sockQuit();
         return 1;
+    } else {
+        printf("connection success!\n");
     }
 
     // Send an initial buffer
     iResult = send( ConnectSocket, sendbuf, (int)strlen(sendbuf), 0 );
     if (iResult == SOCKET_ERROR) {
-        printf("send failed with error: %d\n", WSAGetLastError());
-        closesocket(ConnectSocket);
-        WSACleanup();
+        printf("send failed\n");
+        sockClose(ConnectSocket);
+        sockQuit();
         return 1;
+    } else {
+        printf("send success!\n");
     }
 
-    printf("Bytes Sent: %ld\n", iResult);
-
-    // shutdown the connection since no more data will be sent
-    iResult = shutdown(ConnectSocket, SD_SEND);
-    if (iResult == SOCKET_ERROR) {
-        printf("shutdown failed with error: %d\n", WSAGetLastError());
-        closesocket(ConnectSocket);
-        WSACleanup();
-        return 1;
-    }
+    printf("Bytes Sent: %d\n", iResult);
 
     // Receive until the peer closes the connection
     do {
@@ -114,13 +167,13 @@ int __cdecl main(int argc, char **argv)
         else if ( iResult == 0 )
             printf("Connection closed\n");
         else
-            printf("recv failed with error: %d\n", WSAGetLastError());
+            printf("recv failed\n");
 
     } while( iResult > 0 );
 
     // cleanup
-    closesocket(ConnectSocket);
-    WSACleanup();
+    sockClose(ConnectSocket);
+    sockQuit();
 
     return 0;
 }
--- lab_1/tcp_server_win.c
+++ lab_1/tcp_server_win.c
@@ -1,5 +1,3 @@
-#undef UNICODE
-
 #ifdef _WIN32
   #ifndef _WIN32_WINNT
     #define _WIN32_WINNT 0x0501  /* Windows XP fix found */
